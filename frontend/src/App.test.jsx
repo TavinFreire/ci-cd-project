b1bf7cb1655612b846e54f2287fee410f4842570
TavinFreire <luiz.freire.crc@gmail.com>
2025-08-07 16:59:47 -0300

    Configurações do projeto.

---
+++ frontend/src/App.test.jsx
@@ -0,0 +1,10 @@
+import { describe, it, expect } from "vitest";
+
+describe("testes iniciais", () => {
+    it("teste de soma", () => {
+        expect(2 + 2).toBe(4);
+    });
+    it("teste de subtração", () => {
+        expect(2 - 1).toBe(1);
+    })
+});
